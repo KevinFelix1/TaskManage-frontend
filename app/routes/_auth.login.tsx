62e9ac13d494236f016261c703d95fa2dcbd66c3
KevinFelix1 <kevinfelixm.1@gmail.com>
2024-02-18 03:03:36 -0700

    add shadcn/ui input

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,3 +1,15 @@
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { loginSchemaUS } from "../schemas/auth.schema";
 function Login() {
   return <h1>LoginPage</h1>;
 }
